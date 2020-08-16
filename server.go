dad4501e0f2e5b95829cd99c602215c14d3c6cb2
TheAmazingBrianPowell <22powellbe@gmail.com>
2020-08-15 22:56:38 -0700

    Edited parsing of the text files to HTML

--- server.go
+++ server.go
@@ -8,14 +8,15 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 const contentSecurityPolicyValue = "default-src none; script-src 'self'; img-src 'self'; media-src 'self'; style-src-elem 'self'; style-src 'self';"
 const contentSecurityPolicy = "Content-Security-Policy"
+var lastIP string
 
 func main() {
 	http.HandleFunc("/", mainHandler)
-	http.HandleFunc("/blog/", blogHandler)
 	http.HandleFunc("/resources/", resourceHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -31,25 +32,28 @@ func main() {
 
 func resourceHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set(contentSecurityPolicy, contentSecurityPolicyValue)
+	ip := getIP(r)
+	if lastIP != ip {
+		fmt.Println(" ")
+		lastIP = ip
+	}
 	if fileExists("public/" + r.URL.Path) {
-		http.ServeFile(w, r, "public/"+r.URL.Path)
+		http.ServeFile(w, r, "public/" + r.URL.Path)
+		fmt.Println(ip + strings.Repeat(" ", min(21 - len(ip), 1)) + " | " + r.URL.Path + strings.Repeat(" ", min(40 - len(r.URL.Path), 0)) + " | " + time.Now().Format("2006/01/02 03:04:05.000000 PM") + " | " + "200")
 	} else {
 		write404(w)
+		fmt.Println(ip + strings.Repeat(" ", min(21 - len(ip), 1)) + " | " + r.URL.Path + strings.Repeat(" ", min(40 - len(r.URL.Path), 0)) + " | " + time.Now().Format("2006/01/02 03:04:05.000000 PM") + " | " + "404")
 	}
 }
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
+/*func mainHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set(contentSecurityPolicy, contentSecurityPolicyValue)
 	var (
 		f   *os.File
 		err error
 	)
 	if r.URL.Path != "/" {
-		//if nameLoc := strings.Index(r.URL.Path, "."); nameLoc != -1 {
-		//	f, err = os.Open(r.URL.Path[:nameLoc] + ".html")
-		//} else {
 		f, err = os.Open("public/" + r.URL.Path + ".html")
-		//}
 	} else {
 		f, err = os.Open("public/index.html")
 	}
@@ -71,34 +75,47 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprintf(w, out)
 	}
-}
+}*/
 
-func blogHandler(w http.ResponseWriter, r *http.Request) {
+func mainHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set(contentSecurityPolicy, contentSecurityPolicyValue)
 	out, err := interpretToHTML("public" + r.URL.Path)
+	ip := getIP(r)
+	if lastIP != ip {
+		fmt.Println(" ")
+		lastIP = ip
+	}
 	if err != io.EOF {
 		write404(w)
-		fmt.Println(err)
+		fmt.Println(ip + strings.Repeat(" ", min(21 - len(ip), 0)) + " | " + r.URL.Path + strings.Repeat(" ", min(40 - len(r.URL.Path), 0)) + " | " + time.Now().Format("2006/01/02 03:04:05.000000 PM") + " | " + "404")
+	} else {
+		fmt.Println(ip + strings.Repeat(" ", min(21 - len(ip), 0)) + " | " + r.URL.Path + strings.Repeat(" ", min(40 - len(r.URL.Path), 0)) + " | " + time.Now().Format("2006/01/02 03:04:05.000000 PM") + " | " + "200")
+		fmt.Fprintf(w, out)
 	}
-	fmt.Fprintf(w, out)
 }
 
 func interpretToHTML(filename string) (output string, err error) {
 	var f *os.File
-
-	f, err = os.Open(filename + ".txt")
+	if filename == "public/" {
+		f, err = os.Open("public/index.txt")
+	} else if filename == "public/index" {
+		err = os.ErrNotExist
+	} else {
+		f, err = os.Open(filename + ".txt")
+	}
 	defer f.Close()
 
 	if err != nil {
 		return
 	}
 	b := make([]byte, 1)
-	var title string
-	isCode := 0
+	var isCode int8 = 0
 	isInCode := false
-	started := false
-	var lastChar string
-	firstLine := true
+	var inCommand int8
+	command := ""
+	arg := ""
+	inParagraph := false
+	lineBreak := false
 
 	for {
 		var readTotal int
@@ -116,36 +133,68 @@ func interpretToHTML(filename string) (output string, err error) {
 				isCode++
 			}
 			if isCode == 3 && !isInCode {
+				if inParagraph {
+					inParagraph = false
+					output += "</p>"
+				}
 				isInCode = true
 				output += "<pre>"
+				lineBreak = false
 			} else if isCode == 0 && isInCode {
 				isInCode = false
 				lastPre := strings.LastIndex(output, "<pre>") + 5
 				output = output[:lastPre] + html.EscapeString(output[lastPre:])
 				output += "</pre>"
+				lineBreak = true
 			}
-			lastChar = "`"
-		} else if str == "\n" && !isInCode && lastChar != "\n" {
-			if firstLine {
-				output = "<!DOCTYPE html><html lang='en'><head><meta name='viewport' content='width=device-width, initial-scale=1'><title>" + title + " | The Blob Blog</title><link rel='stylesheet' type='text/css' href='/resources/main.css'></head><body><main><h1>" + title + "</h1>"
-				firstLine = false
+		} else if inCommand == 2 && str == "\n" || (inCommand == 1 && command == "title" && str == "\n") {
+			if inParagraph {
+				output += "</p>"
+				inParagraph = false
+			} else if command == "title" {
+				if arg != "" {
+					output = "<html lang='en'><head><meta name='viewport' content='width=device-width, initial-scale=1'><title>" + arg + " | The Blob Blog</title><link rel='stylesheet' type='text/css' href='/resources/main.css'/></head><body><header><nav><a href='/'>Home</a><a href='/projects'>Projects</a><a href='/tutorials'>Coding Tutorials</a><a href='/music'>Music</a><button><div id='bar1'></div></button></nav></header><main>"
+				} else {
+					output = "<html lang='en'><head><meta name='viewport' content='width=device-width, initial-scale=1'><title>The Blob Blog</title><link rel='stylesheet' type='text/css' href='/resources/main.css'/></head><body><header><nav><a href='/'>Home</a><a href='/projects'>Projects</a><a href='/tutorials'>Coding Tutorials</a><a href='/music'>Music</a><button><div id='bar1'></div></button></nav></header><main>"
+				}
+			} else if command == "img" {
+				output += "<img src = '" + arg + "'>"
+			} else if command == "h1" {
+				output += "<h1>" + arg + "</h1>"
+			} else if command == "h2" {
+				output += "<h2>" + arg + "</h2>"
 			}
-			if started {
-				output += "</p><p>"
-				started = true
+			inCommand = 0
+			command = ""
+			arg = ""
+			lineBreak = true
+		} else if inCommand == 2 {
+			arg += str
+		} else if inCommand == 1 && str == " " {
+			inCommand = 2
+			lineBreak = false
+		} else if inCommand == 1 {
+			command += str
+			lineBreak = false
+		} else if str == "#" {
+			inCommand = 1
+			lineBreak = false
+		} else if str != "\n" || isInCode {
+			output += str
+			lineBreak = false
+		} else if lineBreak && str == "\n" {
+			if inParagraph {
+					output += "</p><p>"
 			} else {
 				output += "<p>"
 			}
-			lastChar = "\n"
-		} else if firstLine {
-			title += str
-		} else if str != "\n" || isInCode {
-			output += str
-			lastChar = str
+			inParagraph = true
+			lineBreak = false
+		} else if str == "\n" {
+			lineBreak = true
 		}
 	}
-
-	output += "</p></main></body></html>"
+	output += "</p></main><footer><p>Copyright © 2020 <strong>Brian E. Powell</strong></p></footer></body></html>"
 	return
 }
 
@@ -160,4 +209,19 @@ func fileExists(filename string) bool {
 func write404(w http.ResponseWriter) {
 	w.WriteHeader(404)
 	fmt.Fprintf(w, "<!DOCTYPE html><html><head><title>Error 404</title><link rel='stylesheet' type='text/css' href='/resources/main.css'></head><body><h1>Error 404: Page Not Found</h1></body></html>")
+}
+
+func getIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return r.RemoteAddr
+}
+
+func min(value int, minimum int) int {
+	if value < minimum {
+		return minimum
+	}
+	return value
 }
